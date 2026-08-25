e3fd811fa2ae3954c085add7dfb94ddb28c073b0
kimmin717 <kimdonghu0202@gmail.com>
2026-08-25 11:54:58 +0900

    Create WayPoints.cs

---
+++ Assets/RunTime/WayPoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    public static Transform[] _points;
+
+    private void Awake()
+    {
+        // childCount : 웨이 포인트 안에 개체들이 배열의 길이가 된다. 내 경우에는 (0 ~ 15) 의 배열을 같는다.
+        _points = new Transform[transform.childCount]; 
+
+
+    }
+
+
+}
