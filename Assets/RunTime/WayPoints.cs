80093da11572eac63a10301aeae73083717c1010
kimmin717 <kimdonghu0202@gmail.com>
2026-08-25 12:56:20 +0900

    Update WayPoints.cs

--- Assets/RunTime/WayPoints.cs
+++ Assets/RunTime/WayPoints.cs
@@ -7,8 +7,11 @@ public class WayPoints : MonoBehaviour
     private void Awake()
     {
         // childCount : 웨이 포인트 안에 개체들이 배열의 길이가 된다. 내 경우에는 (0 ~ 15) 의 배열을 같는다.
-        _points = new Transform[transform.childCount]; 
-
+        _points = new Transform[transform.childCount];
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = transform.GetChild(i);
+        }
 
     }
 
