c3d867aebd665ea7178d3b9d0a60e19e14d7fee6
kimmin717 <kimdonghu0202@gmail.com>
2026-08-26 17:30:45 +0900

    Update TurretSearch.cs

--- Assets/RunTime/Turret/TurretSearch.cs
+++ Assets/RunTime/Turret/TurretSearch.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class TurretSearch : MonoBehaviour
 {
     #region 인스펙터
+    [Header("타겟")]
+    [SerializeField] private Transform _target;
 
+    [Header("범위")]
+    [SerializeField] private float _range = 15f;
     #endregion
 
     #region 내부변수
@@ -21,5 +25,12 @@ public class TurretSearch : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // 원형으로 범위를 보여줌
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _range);
     }
 }
