09c175f09798f45dce462dbd4d9df60d3422f069
kimmin717 <kimdonghu0202@gmail.com>
2026-08-27 14:28:42 +0900

    Create Bullet.cs

---
+++ Assets/RunTime/Bullet/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    #region 인스펙터
+    [Header("타겟")]
+    [SerializeField] private Transform _target;
+    #endregion
+
+    private void Seek()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
